937f7861ee236429a48209432f61225a4d358285
cgonzales20099-cyber <cgonzales20099@iesjoanramis.org>
2026-02-11 13:49:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ document.getElementById("musica").addEventListener ("click", function () {
     canço.play();
 })
 document.getElementById("musica2").addEventListener("click", function () {
-    let canço = new Audio("./musica/audio2.mp3")
+    let canço = new Audio("audio2.mp3")
     canço.play();
 })
 
 
 document.addEventListener("keydown", function (e) {
     if (e.key == "a") {
-        let so = new Audio("./musica/audio1.mp3")
+        let so = new Audio("audio1.mp3")
         so.play();
     }
     if (e.key == "c") {
-        let so2 = new Audio("./musica/audio1.mp3")
+        let so2 = new Audio("audio1.mp3")
         so2.play();
     }
     if (e.key == "b") {
-        let so3 = new Audio("./musica/audio1.mp3")
+        let so3 = new Audio("audio1.mp3")
         so3.play();
     }
 })
