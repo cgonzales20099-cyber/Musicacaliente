e6a8ad75899b5e2a30a5326f3e96257ecb04240e
cgonzales20099-cyber <cgonzales20099@iesjoanramis.org>
2026-02-12 22:33:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
     let canço = new Audio("audio1.mp3");
-        let canço2 = new Audio("audio2.mp3")
+        let canço2 = new Audio("audio2.mp3");
+        let canço3 = new Audio("Cancion3.mp3")
+    document.getElementById("musica3").addEventListener ("click", function () {
+
+    canço3.play();
+})
+   document.getElementById("musica3p").addEventListener ("click", function () {
+
+    canço3.pause();
+})
+
 document.getElementById("musica").addEventListener ("click", function () {
 
     canço.play();
@@ -17,19 +27,43 @@ document.getElementById("musica2p").addEventListener("click", function () {
     canço2.pause();
 })
 
+let so = new Audio("sound1.mp3");
+let so2 = new Audio("sound2.mp3");
+let so3 = new Audio("sound3.mp3");
+let so4 = new Audio("sound4.mp3");
+let so5 = new Audio("sound5_reverse.mp3");
 
 document.addEventListener("keydown", function (e) {
+
     if (e.key == "a") {
-        let so = new Audio("audio1.mp3")
         so.play();
     }
-    if (e.key == "c") {
-        let so2 = new Audio("audio1.mp3")
+
+    if (e.key == "s") {
         so2.play();
     }
-    if (e.key == "b") {
-        let so3 = new Audio("audio1.mp3")
+      if (e.key == "x") {
+        so2.pause();
+    }
+
+    if (e.key == "d") {
         so3.play();
     }
-})
 
+    if (e.key == "c") {
+        so3.pause();   
+    }
+
+    if (e.key == "f") {
+        so4.play();
+    }
+
+    if (e.key == "v") {
+        so4.pause();
+    }
+
+    if (e.key == "g") {
+        so5.play();
+    }
+
+});
